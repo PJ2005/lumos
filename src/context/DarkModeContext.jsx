6d7fa83623832d399d79ae3ec0cb5644fc52129c
PJ2005 <pratham.j2905@gmail.com>
2024-06-20 19:59:33 +0530

    changed dark mode icon

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from 'react';
+// DarkModeContext.jsx
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const DarkModeContext = createContext();
 
@@ -7,9 +8,15 @@ export const useDarkMode = () => useContext(DarkModeContext);
 export const DarkModeProvider = ({ children }) => {
     const [dark, setDark] = useState(false);
 
+    useEffect(() => {
+        const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        setDark(isDarkMode);
+        document.body.classList.toggle('dark', isDarkMode);
+    }, []);
+
     const toggleDarkMode = () => {
         setDark(prevDark => !prevDark);
-        document.body.classList.toggle("dark");
+        document.body.classList.toggle('dark');
     };
 
     return (
